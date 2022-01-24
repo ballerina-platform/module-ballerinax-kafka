8391b75be4b52578cf88e13cee3a29d6922de5ee
dilanSachi <dilansachinthanos@gmail.com>
2022-01-24 14:38:51 +0530

    Add tests for code action

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/kafka/plugin/CodeSnippetGenerationCodeActionTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/kafka/plugin/CodeSnippetGenerationCodeActionTest.java
@@ -31,7 +31,9 @@ import java.util.List;
 import static io.ballerina.stdlib.kafka.plugin.CompilerPluginTestUtils.BALLERINA_SOURCES;
 import static io.ballerina.stdlib.kafka.plugin.CompilerPluginTestUtils.EXPECTED_SOURCES;
 import static io.ballerina.stdlib.kafka.plugin.CompilerPluginTestUtils.RESOURCE_DIRECTORY;
-import static io.ballerina.stdlib.kafka.plugin.KafkaCodeTemplate.NODE_LOCATION;
+import static io.ballerina.stdlib.kafka.plugin.PluginConstants.CODE_TEMPLATE_NAME_WITHOUT_CALLER;
+import static io.ballerina.stdlib.kafka.plugin.PluginConstants.CODE_TEMPLATE_NAME_WITH_CALLER;
+import static io.ballerina.stdlib.kafka.plugin.PluginConstants.NODE_LOCATION;
 
 /**
  * A class for testing code actions.
@@ -48,7 +50,8 @@ public class CodeSnippetGenerationCodeActionTest extends AbstractCodeActionTest 
                 .resolve("service_1")
                 .resolve("result.bal");
         performTest(filePath, LinePosition.from(2, 0),
-                getExpectedCodeAction("service.bal", 2, 64), resultPath);
+                getExpectedCodeAction("service.bal", 2, 64, "Insert service template with caller",
+                        CODE_TEMPLATE_NAME_WITH_CALLER), resultPath);
     }
 
     @Test
@@ -61,15 +64,45 @@ public class CodeSnippetGenerationCodeActionTest extends AbstractCodeActionTest 
                 .resolve("service_2")
                 .resolve("result.bal");
         performTest(filePath, LinePosition.from(2, 0),
-                getExpectedCodeAction("service.bal", 5, 1), resultPath);
+                getExpectedCodeAction("service.bal", 5, 1, "Insert service template with caller",
+                        CODE_TEMPLATE_NAME_WITH_CALLER), resultPath);
     }
 
-    private CodeActionInfo getExpectedCodeAction(String filePath, int line, int offset) {
+    @Test
+    public void testEmptyServiceCodeActionWithoutCaller()
+            throws IOException {
+        Path filePath = RESOURCE_DIRECTORY.resolve(BALLERINA_SOURCES)
+                .resolve("snippet_gen_service_1")
+                .resolve("service.bal");
+        Path resultPath = RESOURCE_DIRECTORY.resolve(EXPECTED_SOURCES)
+                .resolve("service_3")
+                .resolve("result.bal");
+        performTest(filePath, LinePosition.from(2, 0),
+                getExpectedCodeAction("service.bal", 2, 64, "Insert service template without caller",
+                        CODE_TEMPLATE_NAME_WITHOUT_CALLER), resultPath);
+    }
+
+    @Test
+    public void testServiceWithVariablesCodeActionWithoutCaller()
+            throws IOException {
+        Path filePath = RESOURCE_DIRECTORY.resolve(BALLERINA_SOURCES)
+                .resolve("snippet_gen_service_2")
+                .resolve("service.bal");
+        Path resultPath = RESOURCE_DIRECTORY.resolve(EXPECTED_SOURCES)
+                .resolve("service_4")
+                .resolve("result.bal");
+        performTest(filePath, LinePosition.from(2, 0),
+                getExpectedCodeAction("service.bal", 5, 1, "Insert service template without caller",
+                        CODE_TEMPLATE_NAME_WITHOUT_CALLER), resultPath);
+    }
+
+    private CodeActionInfo getExpectedCodeAction(String filePath, int line, int offset,
+                                                 String actionName, String templateName) {
         LineRange lineRange = LineRange.from(filePath, LinePosition.from(2, 0),
                 LinePosition.from(line, offset));
         CodeActionArgument locationArg = CodeActionArgument.from(NODE_LOCATION, lineRange);
-        CodeActionInfo codeAction = CodeActionInfo.from("Insert service template", List.of(locationArg));
-        codeAction.setProviderName("KAFKA_112/ballerinax/kafka/ADD_REMOTE_FUNCTION_CODE_SNIPPET");
+        CodeActionInfo codeAction = CodeActionInfo.from(actionName, List.of(locationArg));
+        codeAction.setProviderName("KAFKA_112/ballerinax/kafka/" + templateName);
         return codeAction;
     }
 }
