eec372e0518860aef76f0a08140eb3c13fefd871
dilanSachi <dilansachinthanos@gmail.com>
2023-03-22 14:29:15 +0530

    Add system.exit to exit the service on panic

--- native/src/main/java/io/ballerina/stdlib/kafka/impl/KafkaListenerImpl.java
+++ native/src/main/java/io/ballerina/stdlib/kafka/impl/KafkaListenerImpl.java
@@ -20,6 +20,7 @@ package io.ballerina.stdlib.kafka.impl;
 
 import io.ballerina.runtime.api.PredefinedTypes;
 import io.ballerina.runtime.api.Runtime;
+import io.ballerina.runtime.api.async.Callback;
 import io.ballerina.runtime.api.async.StrandMetadata;
 import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.ArrayType;
@@ -157,11 +158,28 @@ public class KafkaListenerImpl implements KafkaListener {
         ObjectType serviceType = (ObjectType) TypeUtils.getReferredType(service.getType());
         if (serviceType.isIsolated() && serviceType.isIsolated(KAFKA_RESOURCE_ON_ERROR)) {
             bRuntime.invokeMethodAsyncConcurrently(service, KAFKA_RESOURCE_ON_ERROR, null, metadata,
-                    null, properties, PredefinedTypes.TYPE_NULL, arguments);
+                    getOnErrorCallback(), properties, PredefinedTypes.TYPE_NULL, arguments);
         } else {
             bRuntime.invokeMethodAsyncSequentially(service, KAFKA_RESOURCE_ON_ERROR, null, metadata,
-                    null, properties, PredefinedTypes.TYPE_NULL, arguments);
+                    getOnErrorCallback(), properties, PredefinedTypes.TYPE_NULL, arguments);
         }
+    }
+
+    private Callback getOnErrorCallback() {
+        return new Callback() {
+            @Override
+            public void notifySuccess(Object result) {
+                if (result instanceof BError) {
+                    ((BError) result).printStackTrace();
+                }
+            }
+
+            @Override
+            public void notifyFailure(BError bError) {
+                bError.printStackTrace();
+                System.exit(1);
+            }
+        };
     }
 
     private Map<String, Object> getNewObserverContextInProperties(BObject listener) {
