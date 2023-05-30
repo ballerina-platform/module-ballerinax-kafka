89d1eda496684044f23165793fa3c2c227e734d6
dilanSachi <dilansachinthanos@gmail.com>
2023-05-30 16:03:38 +0530

    Update the logging formatter

--- native/src/main/java/io/ballerina/stdlib/kafka/utils/ModuleUtils.java
+++ native/src/main/java/io/ballerina/stdlib/kafka/utils/ModuleUtils.java
@@ -21,9 +21,13 @@ package io.ballerina.stdlib.kafka.utils;
 import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Module;
 
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import static io.ballerina.stdlib.kafka.utils.KafkaConstants.APACHE_KAFKA_PACKAGE_NAME;
 import static io.ballerina.stdlib.kafka.utils.KafkaConstants.BALLERINA_KAFKA_PACKAGE_NAME;
@@ -54,8 +58,26 @@ public class ModuleUtils {
     public static void initializeLoggingConfigurations() {
         Logger apacheKafkaLogger = Logger.getLogger(APACHE_KAFKA_PACKAGE_NAME);
         Logger balKafkaLogger = Logger.getLogger(BALLERINA_KAFKA_PACKAGE_NAME);
+
+        apacheKafkaLogger.setUseParentHandlers(false);
+        balKafkaLogger.setUseParentHandlers(false);
+
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            private static final String format = "[%1$tc] %2$s - %3$s%n";
+
+            @Override
+            public synchronized String format(LogRecord lr) {
+                return String.format(format, new Date(lr.getMillis()), lr.getLevel().getLocalizedName(),
+                        lr.getMessage());
+            }
+        });
+        apacheKafkaLogger.addHandler(handler);
+        balKafkaLogger.addHandler(handler);
+
         apacheKafkaLogger.setLevel(Level.SEVERE);
         balKafkaLogger.setLevel(Level.WARNING);
+
         LogManager.getLogManager().addLogger(apacheKafkaLogger);
         LogManager.getLogManager().addLogger(balKafkaLogger);
     }
