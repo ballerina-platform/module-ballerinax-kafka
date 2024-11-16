89eede46a62e34fe23236be1638d18d76d7e7f55
hindujaB <bhinduja08@gmail.com>
2024-11-16 22:56:24 +0530

    Fix NPE from isInTransaction

--- native/src/main/java/io/ballerina/stdlib/kafka/nativeimpl/producer/Send.java
+++ native/src/main/java/io/ballerina/stdlib/kafka/nativeimpl/producer/Send.java
@@ -52,10 +52,10 @@ public class Send {
         KafkaTracingUtil.traceResourceInvocation(env, producerObject, record.topic());
         final CompletableFuture<Object> balFuture = new CompletableFuture<>();
         KafkaProducer producer = (KafkaProducer) producerObject.getNativeData(NATIVE_PRODUCER);
+        if (TransactionResourceManager.getInstance().isInTransaction()) {
+            handleTransactions(producerObject);
+        }
         Thread.startVirtualThread(() -> {
-            if (TransactionResourceManager.getInstance().isInTransaction()) {
-                handleTransactions(producerObject);
-            }
             try {
                 producer.send(record, (metadata, e) -> {
                     if (Objects.nonNull(e)) {
